e68b677ea866a1c9682e157b962d6071ece3078e
Muhaab0 <muuhaab@gmail.com>
2023-04-30 02:32:33 +0300

    bss

--- next.config.js
+++ next.config.js
@@ -3,14 +3,13 @@ module.exports = () => {
   const rewrites = () => {
     return [
       {
-        source: "/portfolio/me5a/api/:path*",
+        source: "/me5a/api/:path*",
         destination: "https://portofolionodejs-production.up.railway.app/api/:path*",
       },
     ];
   };
   return {
     rewrites,
-    basePath: '/portfolio'
   }
 }
 

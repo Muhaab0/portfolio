e35426742cf572cd507d77cd2346e82c8860e190
Muhaab0 <muuhaab@gmail.com>
2023-04-30 00:50:34 +0300

    b

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = () => {
   swcMinify: true
-  basePath: "/portofolio"
+  basePath: "/PortofolioFront"
   const rewrites = () => {
     return [
       {

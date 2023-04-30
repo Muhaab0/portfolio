a1b51ccd761bcf307d8c35d47c9ed77cf2e8b076
Muhaab0 <muuhaab@gmail.com>
2023-04-30 09:25:02 +0300

    bss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 'use client';
-import Head from 'next/head';
 import Layout from '../component/Layout'
 import '@/styles/globals.css'
 import '../styles/Navbar.css'
@@ -13,6 +12,8 @@ import '../styles/Contact.css'
 import '../styles/AdminData.css'
 import { AuthProvider } from '@/Context/Auth.Context'
 import { QueryClient, QueryClientProvider } from "react-query"
+import Head from 'next/head';
+import favicon from '../public/assets/favicon.ico';
 
 export default function App({ Component, pageProps }) {
 
@@ -22,10 +23,7 @@ export default function App({ Component, pageProps }) {
     
     <>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png"/>
-        <link rel="manifest" href="/fav/site.webmanifest"/>
+      <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
     </Head>
     <QueryClientProvider client={queryClient}>
     <AuthProvider>
